cbd54d9a1c5baf7f92101e8af413f4574de12517
Liyuan_Zhao <Liyuan_Zhao@human-horizons.com>
2022-08-23 11:23:26 +0800

    Rename PracticesApplication

--- app/src/main/java/com/tsdl/practices/PracticesApplication.java
+++ app/src/main/java/com/tsdl/practices/PracticesApplication.java
@@ -1,6 +1,8 @@
 package com.tsdl.practices;
 
-public class BaseApplication extends com.tmec.common.sdk.base.BaseApplication {
+import com.tmec.common.sdk.base.BaseApplication;
+
+public class PracticesApplication extends BaseApplication {
 
     @Override
     public void onCreate() {
